32757dd9b4bde9b1cbe9f4b4e8996dbdfffa6918
Полина Полянская <ppolina353@yandex.ru>
2022-11-03 15:04:12 +0300

    add __cmp__ но зачем...

--- 4 практика/zadanie5.py
+++ 4 практика/zadanie5.py
@@ -3,10 +3,13 @@ class RealString:
         self.word = word 
     def __len__(self):
         return len(self.word)
-    def __eq__(self, b):
-        return len(self.word) == len(b) 
-    def __ne__(self, b):
-        return len(self.word) != len(b)
+    def __cmp__(self, b): #для == и !=
+        if len(self.word) > len(b):
+            return -1
+        elif len(self.word) < len(b):   
+            return 1
+        else:
+            return 0
     def __lt__(self, b):
         return len(self.word) < len(b) 
     def __gt__(self, b):
@@ -14,7 +17,12 @@ class RealString:
     def __le__(self, b):
         return len(self.word) <= len(b)
     def __ge__(self, b):
-        return len(self.word) >= len(b)      
+        return len(self.word) >= len(b)   
+    def __eq__(self, b):
+        return len(self.word) == len(b) 
+'''    def __ne__(self, b):
+        return len(self.word) != len(b)'''
+
     
 a = RealString('привет')
 b = RealString('ййй')
@@ -25,4 +33,4 @@ print(a <= b)
 print(a > b)
 print(a < b)
 print(a > 'яяя')
-print(b == 'щщщ')
+print(b == 'aaa')
